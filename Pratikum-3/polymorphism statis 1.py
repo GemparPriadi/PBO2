663862fdd72b7b9de7b3cf66449f0e222e0c6824
Gempar Priadi <116055415+GemparPriadi@users.noreply.github.com>
2023-04-03 22:12:26 +0700

    Add files via upload

---
+++ Pratikum-3/polymorphism statis 1.py
@@ -0,0 +1,7 @@
+# Nama  : Gempar Priadi
+# Kelas : R1/T21A
+# NIM   : 210511036
+
+print(5 - 1)
+print("Above" + " The" + " Clouds")
+print([1, 2] + [3, 4]) 
---
+++ Pratikum-3/polymorphism statis 2.py
@@ -0,0 +1,6 @@
+# Nama  : Gempar Priadi
+# Kelas : R1/T21A
+# NIM   : 210511036
+
+print(len("geeks"))
+print(len([10, 20, 30]))
